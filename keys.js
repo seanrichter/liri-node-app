8d69e14ca1cf18c4129483c2d6e443e72ba97173
Sean Richter <sean.richter04@outlook.com>
2019-09-29 15:52:14 -0500

    cleaning up indention

--- keys.js
+++ keys.js
@@ -3,8 +3,8 @@ console.log('this is loaded');
 var dotenv = require("dotenv").config();
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,  
-    secret: process.env.SPOTIFY_SECRET,   
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET,
 };
 
 exports.bands = {
